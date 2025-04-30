2c25242533b9d26cbfd46c068dad344c05b0b89f
CBW16 <w1983107@westminster.ac.uk>
2025-04-30 17:25:08 +0100

    Update app.py

--- app.py
+++ app.py
@@ -5,8 +5,10 @@ import matplotlib.pyplot as plt
 st.title("Welcome to my dashboard!")
 st.checkbox('Click here to start') #allows user to interact with a checkbox
 #add preview
-df = pd.read_csv("/workspaces/Lifecyclecw/Licensed_Drivers.csv")
+df = pd.read_csv("Licensed_Drivers.csv")
 st.write("First few rows:")
 st.dataframe(df.head())
 
 st.subheader("Total Licensed Drivers by Age Group (2015-2017)")
+
+
