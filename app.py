7cee9e349b634a92e78ea24ccd466c9214081a49
CBW16 <w1983107@westminster.ac.uk>
2025-05-01 01:49:34 +0100

    Updating my title and adding a preview feature for my data

--- app.py
+++ app.py
@@ -2,12 +2,13 @@ import pandas as pd
 import streamlit as st
 import matplotlib.pyplot as plt
 
-st.title("Welcome to my dashboard!")
+st.title("Licensed Drivers in the US Dashboard")
 st.checkbox('Click here to start') #allows user to interact with a checkbox
-#add preview
+
 df = pd.read_csv("Licensed_Drivers.csv")
-st.write("First few rows:")
-st.dataframe(df.head())
+
+with st.expander("Data Preview"): #preview of data for user
+    st.dataframe(df.head())
 
 st.subheader("Total Licensed Drivers by Age Group (2015-2017)")
 
