0dae3ddb50ce74f324f88b625029c9ea9698c4a4
CBW16 <w1983107@westminster.ac.uk>
2025-05-02 01:54:45 +0000

    Removed unamed columns from my preview. Adjusted some of my data for my my first visualisation.

--- app.py
+++ app.py
@@ -1,15 +1,19 @@
 import pandas as pd
 import streamlit as st
 import matplotlib.pyplot as plt
+import seaborn as sns
 
 st.title("Licensed Drivers in the US Dashboard")
 st.checkbox('Click here to start') #allows user to interact with a checkbox
 
 df = pd.read_csv("Licensed_Drivers.csv")
+df = df.drop(columns=[col for col in df.columns if "Unnamed" in col])#removing unamed columns
 
 with st.expander("Data Preview"): #preview of data for user
-    st.dataframe(df.head())
+    st.dataframe(df)
 
 st.subheader("Total Licensed Drivers by Age Group (2015-2017)")
 
-
+drivers_by_year = df.groupby("Year")["Drivers"].sum() #total drivers count for each year 
+drivers_by_cohort = df.groupby("Cohort")["Drivers"].sum().sort_values(ascending=False) 
+#adds up every driver count in each age group
