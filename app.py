565eab797be06ad4403c531abe5bdd252ada496a
CBW16 <w1983107@westminster.ac.uk>
2025-05-02 17:48:20 +0000

    Added my first visualisation

--- app.py
+++ app.py
@@ -17,3 +17,9 @@ st.subheader("Total Licensed Drivers by Age Group (2015-2017)")
 drivers_by_year = df.groupby("Year")["Drivers"].sum() #total drivers count for each year 
 drivers_by_cohort = df.groupby("Cohort")["Drivers"].sum().sort_values(ascending=False) 
 #adds up every driver count in each age group
+
+fig, ax = plt.subplots(figsize=(8, 7)) #change to find optimal size
+sns.barplot(x=drivers_by_cohort.values, y=drivers_by_cohort.index, palette="Greens_d", ax=ax)
+ax.set_xlabel("Number of Drivers") 
+ax.set_ylabel("Age Group")
+st.pyplot(fig)
