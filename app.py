5057a3db8633955c965646620e290e05e12f642b
CBW16 <w1983107@westminster.ac.uk>
2025-05-03 01:32:28 +0000

    I finalised my first visualisation. I had to adjust the size and the x-axis scale to ensure it displayed values correctly. I almost finished my second vizualisation but I still need to debug it to get rid of several errors.

--- app.py
+++ app.py
@@ -14,12 +14,33 @@ with st.expander("Data Preview"): #preview of data for user
 
 st.subheader("Total Licensed Drivers by Age Group (2015-2017)")
 
-drivers_by_year = df.groupby("Year")["Drivers"].sum() #total drivers count for each year 
-drivers_by_cohort = df.groupby("Cohort")["Drivers"].sum().sort_values(ascending=False) 
+drivers_by_year = df.groupby("Year")["Drivers"].sum() / 1e6 #total drivers count for each year 
+drivers_by_cohort = df.groupby("Cohort")["Drivers"].sum().sort_values(ascending=False) / 1e6 
 #adds up every driver count in each age group
 
-fig, ax = plt.subplots(figsize=(8, 7)) #change to find optimal size
+fig, ax = plt.subplots(figsize=(10, 7)) #change to find optimal size
 sns.barplot(x=drivers_by_cohort.values, y=drivers_by_cohort.index, palette="Greens_d", ax=ax)
-ax.set_xlabel("Number of Drivers") 
+ax.set_xlabel("Number of Drivers(Millions)") 
 ax.set_ylabel("Age Group")
 st.pyplot(fig)
+
+
+st.title("Total Drivers by State")
+
+df = df[['Year', 'State', 'Drivers']].astype({'Year': 'int', 'Drivers': 'int'}) #ensure correct types especially for year
+
+
+year = st.slider(  #allows user to select a year using a slider
+    "Select Year",
+    min_value=int(df.Year.min()),
+    max_value=int(df.Year.max()),
+    value=int(df.Year.min())
+)
+
+
+df_year = df[df.Year == year]
+by_state = df_year.groupby('State')['Drivers'].sum()
+
+st.subheader(f"Licensed Drivers in Year") #shows updated chart as soon as user uses the slider
+st.bar_chart(by_state)
+
